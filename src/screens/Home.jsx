da746979d7366c76617301bd8cf40c2000352b55
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-01-23 04:31:04 +0500

    "Day-02" for Food-Deleiver-App     Using Routers     Design React Components

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Card from '../components/Card'
+import Carousel from '../components/Carousel'
 
 const Home = () => {
   return (
     <div>
 
         <div><Navbar/></div>
-        <div>Body</div>
+        <div><Carousel/></div>
+        <div className='m-3'>
+          <Card/>
+          </div>
         <div><Footer/></div>
     </div>
   )
