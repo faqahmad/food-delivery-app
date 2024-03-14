8fb13518c4cc484b6e47c5016604c3ef1954d132
AfaaqRamzan <69888091+AfaaqRamzan@users.noreply.github.com>
2024-03-14 14:35:11 +0500

    "Day-04 Model, Schema and Riutes in Mongoose"

--- backend/db.js
+++ backend/db.js
@@ -1,23 +1,18 @@
 const mongoose = require('mongoose');
-const mongoURI = 'mongodb+srv://gofood:iahPGZUGmRiY6xlf@cluster0.di95dll.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const mongoURI = 'mongodb+srv://gofood:iahPGZUGmRiY6xlf@cluster0.di95dll.mongodb.net/gofoodmern?retryWrites=true&w=majority&appName=Cluster0'
 
-//  OLD WAY TO CONNECT TO MONGODB
-// const mongoDB = async () => {
-//     await mongoose.connect(mongoURI, { useNewUrlParser: true }, (err, result) => {
-//         if(err) console.log("---",err)
-//         else {
-//             console.log('connected');
-
-//         }
-//     });
-// }
-
-// NEW WAY TO CONNECT MONGODB
 const mongoDB = async () => {
-    await mongoose.connect(mongoURI);
-    console.log("Connected to MongoDB");
-};
+    await mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
+        if (err) console.log("...", err)
+        else {
+            console.log("Connected to MongoDB");
+            const fetched_data = await mongoose.connect.db.collection("food_items");
+            fetched_data.find({}).toArray(function(err, data){
+                if(err) console.log(err);
+                else console.log(data);
+            })
+        }
+    });
+}
 
-// mongoDB();
-
-module.exports = mongoDB;
+module.exports = mongoDB();
